4eb36f9af838b9209f623bc42372dbaea876eec2
AlexProMaster <125411071+AlexProMaster@users.noreply.github.com>
2023-04-25 14:45:19 -0600

    Api

---
+++ OpticaPopular.Backend/OpticaPopular.DataAccess/Repositories/RequestStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace OpticaPopular.DataAccess.Repositories
+{
+    public class RequestStatus
+    {
+        public int CodeStatus { get; set; }
+        public string MessageStatus { get; set; }
+    }
+}
