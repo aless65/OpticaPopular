8438f50a82b9d6e06d6a8f5bf99ff0c4b296c15f
aless65 <alessia88@hotmail.com>
2023-04-26 22:24:29 -0600

    jjjj

--- OpticaPopularBackend/OpticaPopular.API/Startup.cs
+++ OpticaPopularBackend/OpticaPopular.API/Startup.cs
@@ -29,30 +29,66 @@ namespace OpticaPopular.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            AddSwagger(services);
             services.DataAcces(Configuration.GetConnectionString("ConnectionDB"));
             services.BusinessLogic();
             services.AddAutoMapper(x => x.AddProfile<MappingProfileExtensions>(), AppDomain.CurrentDomain.GetAssemblies());
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddSession();
+
+            services.AddControllersWithViews();
+
+            services.AddCors(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpticaPopular.API", Version = "v1" });
+                options.AddPolicy("AllowFlutterApp", builder =>
+                {
+                    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+
+                    //builder.WithOrigins("http://localhost:61025/#/"); // Reemplaza con la URL de tu aplicación Flutter
+                });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"Foo {groupName}",
+                    Version = groupName,
+                    Description = "Optica API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Optica Popular",
+                        Email = string.Empty,
+                        Url = new Uri("https://foo.com/"),
+                    }
+                });
+            });
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OpticaPopular.API v1"));
             }
 
             app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foo API V1");
+            });
 
+
+            app.UseRouting();
+            app.UseCors("AllowFlutterApp");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -60,5 +96,75 @@ namespace OpticaPopular.API
                 endpoints.MapControllers();
             });
         }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        //public IConfiguration Configuration { get; }
+
+        //// This method gets called by the runtime. Use this method to add services to the container.
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.DataAcces(Configuration.GetConnectionString("ConnectionDB"));
+        //    services.BusinessLogic();
+        //    services.AddAutoMapper(x => x.AddProfile<MappingProfileExtensions>(), AppDomain.CurrentDomain.GetAssemblies());
+        //    services.AddControllers();
+        //    //services.AddSwaggerGen(c =>
+        //    //{
+        //    //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpticaPopular.API", Version = "v1" });
+        //    //});
+        //    AddSwagger(services);
+        //    services.AddControllersWithViews();
+
+        //}
+
+        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //private void AddSwagger(IServiceCollection services)
+        //{
+        //    services.AddSwaggerGen(options =>
+        //    {
+        //        var groupName = "v1";
+
+        //        options.SwaggerDoc(groupName, new OpenApiInfo
+        //        {
+        //            Title = $"Foo {groupName}",
+        //            Version = groupName,
+        //            Description = "Optica API",
+        //            Contact = new OpenApiContact
+        //            {
+        //                Name = "Optica Popular",
+        //                Email = string.Empty,
+        //                Url = new Uri("https://foo.com/"),
+        //            }
+        //        });
+        //    });
+        //}
+
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+
+        //    app.UseHttpsRedirection();
+
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI(c =>
+        //    {
+        //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foo API V1");
+        //    });
+
+
+        //    app.UseRouting();
+        //    //app.UseCors("AllowFlutterApp");
+        //    app.UseAuthorization();
+
+        //    app.UseEndpoints(endpoints =>
+        //    {
+        //        endpoints.MapControllers();
+        //    });
+        //}
     }
 }
--- prueba-apli-react/src/App.js
+++ prueba-apli-react/src/App.js
@@ -6,24 +6,62 @@ class App extends Component {
     this.state = {
       posts: []
     }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    var jsonData = {
+      "usua_NombreUsuario": "alan",
+      "usua_Contrasena": "contra123",
+      "usua_EsAdmin": false,
+      "role_Id": 1,
+      "empe_Id": 0,
+      "usua_UsuCreacion": 1,
+    }
+
+    // Send data to the backend via POST
+    fetch('http://opticapopular.somee.com/api/Usuarios/Insertar', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(jsonData)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
+
   }
 
   componentDidMount() {
-    const url = "http://www.opticapopular.somee.com/api/Usuarios/Listado";
+    const url = "http://opticapopular.somee.com/api/Usuarios/Listado";
     fetch(url)
       .then(response => response.json())
-      .then(json => this.setState({ posts: json.data}))
+      .then(json => this.setState({ posts: json.data }))
   }
+
 
   render() {
     const { posts } = this.state;
+
     return (
       <div className="container">
         <h1>La primera apli papá</h1>
 
+        <button onClick={this.handleClick} style={{
+          textAlign: 'center',
+          width: '100px',
+          border: '1px solid gray',
+          borderRadius: '5px'
+        }}>
+          Send data to backend
+        </button>
+
         {posts.map((post) => (
           <div className="card" key={post.usua_Id}>
-            ID: {post.usua_Id} TITULO: {post.usua_Contrasena}
+            ID: {post.usua_Id} TITULO: {post.usua_NombreUsuario}
             <div className="card-header">
               <div className="card-body">
                 <img src="https://pbs.twimg.com/media/Dca9i1rUQAEUkTL?format=jpg&name=small" alt="deja de chingar porfa"></img>
