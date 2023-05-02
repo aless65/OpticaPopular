9fe47a923e5d01ca5f59823fa0d083c21f2c0411
AlexProMaster <125411071+AlexProMaster@users.noreply.github.com>
2023-05-02 14:02:56 -0600

    plantilla funcional

---
+++ minimal-react-app-330/src/hooks/useLocales.js
@@ -0,0 +1,31 @@
+import { useTranslation } from 'react-i18next';
+// '@mui
+import { esES } from '@mui/material/locale';
+
+// ----------------------------------------------------------------------
+
+const LANGS = [
+  {
+    label: 'Español',
+    value: 'es',
+    systemValue: esES,
+    icon: 'https://minimal-assets-api.vercel.app/assets/icons/ic_flag_en.svg',
+  }
+];
+
+export default function useLocales() {
+  const { i18n, t: translate } = useTranslation();
+  const langStorage = localStorage.getItem('i18nextLng');
+  const currentLang = LANGS.find((_lang) => _lang.value === langStorage) || LANGS[1];
+
+  const handleChangeLanguage = (newlang) => {
+    i18n.changeLanguage(newlang);
+  };
+
+  return {
+    onChangeLang: handleChangeLanguage,
+    translate,
+    currentLang,
+    allLang: LANGS,
+  };
+}
